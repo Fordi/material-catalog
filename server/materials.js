bab9f3db530361350d61358d5549d03f1659304d
Bryan Elliott <fordiman@gmail.com>
2023-07-09 20:23:08 -0400

    Changes prior to unit tests

--- server/materials.js
+++ server/materials.js
@@ -1,36 +1,34 @@
-const { dirname, join } = require('path');
 const { readFile, writeFile } = require('fs/promises');
 
-const DATA_FILE = join(dirname(__dirname), 'data/materials.json');
 
-async function get(id) {
-  const data = JSON.parse(await readFile(DATA_FILE, 'utf8'));
+async function get(data_file, id) {
+  const data = JSON.parse(await readFile(data_file, 'utf8'));
   if (!!id) {
     return { ...data[id], id };
   }
   return Object.keys(data).map((id) => ({ ...data[id], id }));
 };
 
-async function post({ name, color, volume, cost, deliveryDate }) {
-  const data = JSON.parse(await readFile(DATA_FILE, 'utf8'));
+async function post(data_file, { name, color, volume, cost, deliveryDate }) {
+  const data = JSON.parse(await readFile(data_file, 'utf8'));
   const id = Math.random().toString(36).substring(2);
   data[id] = { name, color, volume, cost, deliveryDate };
-  await writeFile(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');
+  await writeFile(data_file, JSON.stringify(data, null, 2), 'utf8');
   return { ...data[id], id };
 }
 
-async function put(id, update) {
-  const data = JSON.parse(await readFile(DATA_FILE, 'utf8'));
+async function put(data_file, id, update) {
+  const data = JSON.parse(await readFile(data_file, 'utf8'));
   if (!id) {
     throw new Error('Method PUT must contain an ID');
   }
   data[id] = { ...data[id], ...update };
-  await writeFile(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');
-  return { ...materials[id], id };
+  await writeFile(data_file, JSON.stringify(data, null, 2), 'utf8');
+  return { ...data[id], id };
 }
 
-async function remove(id) {
-  const data = JSON.parse(await readFile(DATA_FILE, 'utf8'));
+async function remove(data_file, id) {
+  const data = JSON.parse(await readFile(data_file, 'utf8'));
   if (!id) {
     throw new Error('Method DELETE must contain an ID');
   }
@@ -38,30 +36,31 @@ async function remove(id) {
     throw new Error(`Unknown ID: ${id}`);
   }
   delete data[id];
-  await writeFile(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');
+  await writeFile(data_file, JSON.stringify(data, null, 2), 'utf8');
   return null;
 
 }
-
-async function materials({ body, method, path }, response) {
-  let id = path.replace(/^\//, '') || null;
-  try {
-    switch (method) {
-      case 'GET': return response.json(await get(id));
-      case 'POST':
-        if (!!id) {
-          throw new Error('Cannot use an ID with POST.');
-        }      
-        return response.json(await post(body));
-      case 'PUT': return response.json(await put(id, body));
-      case 'DELETE': return response.json(await remove(id));
-      default:
-        throw new Error(`Unsupported method: ${method}`);
+function endpoint(data_file) {
+  return async function materials({ body, method, path }, response) {
+    let id = path.replace(/^\//, '') || null;
+    try {
+      switch (method) {
+        case 'GET': return response.json(await get(data_file, id));
+        case 'POST':
+          if (!!id) {
+            throw new Error('Cannot use an ID with POST.');
+          }      
+          return response.json(await post(data_file, body));
+        case 'PUT': return response.json(await put(data_file, id, body));
+        case 'DELETE': return response.json(await remove(data_file, id));
+        default:
+          throw new Error(`Unsupported method: ${method}`);
+      }
+    } catch (e) {
+      response.status(500);
+      response.write(e.message);
     }
-  } catch (e) {
-    response.status(500);
-    response.write(e.message);
-  }
+  };
 };
 
-module.exports = materials;
+module.exports = endpoint;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,11 @@
 // Cheap mocked service leveraging setupProxy
 const bodyParser = require('body-parser');
 const materials = require('../server/materials.js');
+const { dirname, join } = require('path');
+
+const DATA_FILE = join(dirname(__dirname), 'data/materials.json');
 
 module.exports = app => {
   app.use(bodyParser.json());
-  app.use('/materials', materials);
+  app.use('/materials', materials(DATA_FILE));
 };
